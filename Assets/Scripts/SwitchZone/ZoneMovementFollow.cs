77ef442950848a96170af77d9d2538892f18881f
Dulst <dulstaminos@163.com>
2026-05-09 20:34:26 +0800

    feat: 为预览区域跟随脚本添加自动绑定功能

--- Assets/Scripts/SwitchZone/ZoneMovementFollow.cs
+++ Assets/Scripts/SwitchZone/ZoneMovementFollow.cs
@@ -6,7 +6,20 @@ public class ZoneMovementFollow : ZoneMovementBase
     public Transform Target;
 
     [Tooltip("跟随偏移量")]
-    public Vector3 Offset;
+    public Vector3 Offset = Vector3.zero;
+
+    private void Start()
+    {
+        if (Target == null)
+        {
+            // 自动寻找并绑定玩家，玩家需有"Player"Tag
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            if (player != null)
+            {
+                Target = player.transform;
+            }
+        }
+    }
 
     protected override void HandleMovement()
     {
