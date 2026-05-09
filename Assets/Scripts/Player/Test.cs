e6cac3faadc969fe4a97bef0591c3358072da48d
Dulst <dulstaminos@163.com>
2026-05-09 20:44:55 +0800

    test: 提供测试用脚本，按K键快速切换世界

---
+++ Assets/Scripts/Player/Test.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            LevelWorldManager.Instance.RequestTotalSwitch();
+        }
+    }
+}
